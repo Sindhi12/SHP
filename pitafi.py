94635cf6de505b85f53d2570bbd6c382aae5efe5
Sindhi12 <87424433+Sindhi12@users.noreply.github.com>
2021-09-25 18:49:32 +0500

    Create pitafi.py
    got clone https://github.com/Sindhi12/SHP.git
    ls
    cd pitafi
    python2 pitafi.py

---
+++ pitafi.py
@@ -0,0 +1,17 @@
+#Facebook : SAJJAD
+#Github   : https://github.com/Sindhi12/SHP.git
+#Whatsapp : +923023280599
+clear
+echo
+echo "Install All You Need, Patient Bro ..!"
+echo
+apt update && apt upgrade
+
+apt install python2
+pip2 install mechanize
+pip2 install requests bs4
+apt install git
+pip2 install futures
+termux-setup-storage
+rm -rf cmbf
+git clone https://github.com/juttbadshah6969/cmbf/
